af9be447cde2e1d91eb6b5f06c6ec49e4e8ab70e
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-02 09:43:10 +0700

    Create SpringBootMsApplication.java

---
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/SpringBootMsApplication.java
@@ -0,0 +1,13 @@
+package fpoly.learn.springbootms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMsApplication.class, args);
+	}
+
+}
