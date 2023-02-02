4597bc8afa40edadeffddf68b00d9e34223d8807
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-02 09:43:16 +0700

    Create SpringBootMsApplicationTests.java

---
+++ spring-boot-ms/src/test/java/fpoly/learn/springbootms/SpringBootMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fpoly.learn.springbootms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
