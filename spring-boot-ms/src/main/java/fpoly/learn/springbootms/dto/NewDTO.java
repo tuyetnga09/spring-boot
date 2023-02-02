a2d19e1270aa9a3476e23e368310a36071e6cc6a
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-02 11:11:07 +0700

    Create NewDTO.java

---
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/dto/NewDTO.java
@@ -0,0 +1,23 @@
+package fpoly.learn.springbootms.dto;
+
+//Class chứa các thông tin chung của 1 trang web
+public class NewDTO {
+    private String title;
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
