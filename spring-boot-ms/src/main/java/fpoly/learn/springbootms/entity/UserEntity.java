df4a967e93a4fea686f0fd375e4217dd61a1cefa
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-03 00:14:05 +0700

    Update UserEntity.java

--- spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/UserEntity.java
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/UserEntity.java
@@ -1,4 +1,52 @@
 package fpoly.learn.springbootms.entity;
 
-public class UserEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class UserEntity extends BaseEntity {
+    @Column(name = "username")
+    private String userName;
+    @Column(name = "password")
+    private String passWord;
+    @Column(name = "fullname")
+    private String fullName;
+    @Column(name = "status")
+    private Integer status;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 }
