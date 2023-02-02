17baddb754bbc01c858666e8c98ca300ba7aec32
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-02 11:11:05 +0700

    Update NewAPI.java

--- spring-boot-ms/src/main/java/fpoly/learn/springbootms/api/NewAPI.java
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/api/NewAPI.java
@@ -1,5 +1,19 @@
 package fpoly.learn.springbootms.api;
+import org.springframework.web.bind.annotation.*;
+import fpoly.learn.springbootms.dto.NewDTO;
+//Tại sao ở đây lại có @RestController
+//RestController: để định nghĩa đây là 1 web-service. Nếu không đây là 1 java class.
+//GetMapping: giúp chúng ta nhận request. ("/test": URL)
 
+@RestController
 public class NewAPI {
+//    @GetMapping("/test")
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
 
+    @ResponseBody
+    public NewDTO createNew(@RequestBody NewDTO model) {
+
+        return model;
+
+    }
 }
