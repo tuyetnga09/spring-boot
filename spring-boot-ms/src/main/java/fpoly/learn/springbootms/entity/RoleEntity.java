cd3d1eb129793a71026733b7578f27932c5426d2
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-03 00:13:59 +0700

    Update RoleEntity.java

--- spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/RoleEntity.java
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/RoleEntity.java
@@ -1,4 +1,31 @@
 package fpoly.learn.springbootms.entity;
 
-public class RoleEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+@Entity
+@Table(name = "role")
+public class RoleEntity extends BaseEntity{
+    @Column(name = "code")
+    private String code;
+    @Column(name = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
