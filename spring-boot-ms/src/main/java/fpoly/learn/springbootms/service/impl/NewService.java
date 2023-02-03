3a9ef6f7cafedf7768216e9576d49024fdcebfd9
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-03 12:09:29 +0700

    Update NewService.java

--- spring-boot-ms/src/main/java/fpoly/learn/springbootms/service/impl/NewService.java
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/service/impl/NewService.java
@@ -1,6 +1,14 @@
 package fpoly.learn.springbootms.service.impl;
 
+import fpoly.learn.springbootms.repository.NewRepository;
 import fpoly.learn.springbootms.service.INewService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class NewService implements INewService {
+
+    //Nhúng Repository để sử dụng --> sd: @Autowired
+    //Để thực thi được cơ chế @Autowired: cần phải có những dấu hiệu, ký hiệu để @Autowired  
+    @Autowired
+    private NewRepository newRepository;
+
 }
