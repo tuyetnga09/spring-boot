a868567b6bb595617feed494abd72d340ee3637b
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-03 10:44:55 +0700

    Update RoleEntity.java

--- spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/RoleEntity.java
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/RoleEntity.java
@@ -2,9 +2,13 @@ package fpoly.learn.springbootms.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 @Entity
 @Table(name = "role")
 public class RoleEntity extends BaseEntity{
@@ -12,6 +16,10 @@ public class RoleEntity extends BaseEntity{
     private String code;
     @Column(name = "name")
     private String name;
+
+    //MappedBy đại diện cho tên list định nghĩa ở bên User
+    @ManyToMany(mappedBy = "roles")
+    private List<UserEntity> user = new ArrayList<>();
 
     public String getName() {
         return name;
@@ -27,5 +35,13 @@ public class RoleEntity extends BaseEntity{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<UserEntity> getUser() {
+        return user;
+    }
+
+    public void setUser(List<UserEntity> user) {
+        this.user = user;
     }
 }
