35b595fff17c4f664a20a7a817a43c59603683f8
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-03 11:36:09 +0700

    Update NewDTO.java

--- spring-boot-ms/src/main/java/fpoly/learn/springbootms/dto/NewDTO.java
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/dto/NewDTO.java
@@ -5,6 +5,11 @@ public class NewDTO {
     private String title;
     private String content;
 
+    private String shortDescription;
+    private String categoryCode;
+    private String thumbnail;
+
+
     public String getContent() {
         return content;
     }
@@ -19,5 +24,29 @@ public class NewDTO {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(String categoryCode) {
+        this.categoryCode = categoryCode;
     }
 }
