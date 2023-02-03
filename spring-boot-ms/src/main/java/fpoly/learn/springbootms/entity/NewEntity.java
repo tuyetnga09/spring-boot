9ce93964d7370d389f9b395eb2dbe3bd380d1154
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-03 10:20:06 +0700

    Update NewEntity.java

--- spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/NewEntity.java
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/NewEntity.java
@@ -1,4 +1,52 @@
 package fpoly.learn.springbootms.entity;
 
-public class NewEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+@Entity
+@Table(name = "news")
+public class NewEntity extends BaseEntity{
+    @Column(name = "title")
+    private String title;
+    @Column(name = "thumbnail")
+    private String thumbnail;
+    @Column(name = "shortDescription")
+    private String shortDescription;
+    @Column(name = "content")
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
 }
