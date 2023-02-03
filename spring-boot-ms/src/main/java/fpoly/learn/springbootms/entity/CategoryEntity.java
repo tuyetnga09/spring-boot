861819e71bc740111a82613397d24c6efbfd36cb
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-02-03 10:20:03 +0700

    Update CategoryEntity.java

--- spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/CategoryEntity.java
+++ spring-boot-ms/src/main/java/fpoly/learn/springbootms/entity/CategoryEntity.java
@@ -1,4 +1,31 @@
 package fpoly.learn.springbootms.entity;
 
-public class CategoryEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+@Entity
+@Table(name = "category")
+public class CategoryEntity extends BaseEntity{
+    @Column(name = "code")
+    private String code;
+    @Column(name = "name")
+    private String name;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
